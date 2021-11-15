9091ba26e281f9cbf0dc6a7508abc4eee6d08c57
bayel04 <93245123+bayel04@users.noreply.github.com>
2021-11-15 22:43:38 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Фоновое изображение</title>
+
+ </head>
+ <body bgcolor=white background="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzER0hbcawNiuRtjRCU9Nu09NHIMcM0l8r0A&usqp=CAU">
+
+<h1>PORTFOLIO</h1>
+
+<hr>
+
+<h2>Personal data</h2>
+
+<p><img src="https://www.meme-arsenal.com/memes/a13202136f3ed3b2ea0135fc70d4d239.jpg" alt="portfolio image" width="220" height="200"></p>
+
+<hr>
+
+<p>Name.  :   Bayel      </p>
+<hr>
+<p>Surname:   Askaruulu     </p>
+<hr>
+<p>Age    :   16             </p>
+<hr>
+<p>Class  :.  Csc-20a.       </p>
+<hr>
+
+</body>
+</html>

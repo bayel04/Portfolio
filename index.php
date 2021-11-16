6e2fb714c1bea34e160d43304bcf3e8e61051b3e
bayel04 <93245123+bayel04@users.noreply.github.com>
2021-11-16 14:09:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
   <meta charset="utf-8">
   <title>Круглые изображения</title>
   <style>
+   .parent {
+  padding: 10px;
+}
+img {
+  position: relative;
+  top: 80%; 
+  left: 80%;
+  height: 20%;
+  width: 20%;
+}
    .round {
     border-radius: 10000px; /* Радиус скругления */
     border: px solid green; /* Параметры рамки */
@@ -13,19 +23,7 @@
   </style>
  </head>
  <body>
-  #content {
-    position: relative;
-}
-#content img {
-    position: absolute;
-    top: 0px;
-    right: 0px;
-}
 
-<div id="content">
-    <img src="images/ribbon.png" class="ribbon"/>
-    <div>some text...</div>
-</div>
 
    <p><img src="https://i.pinimg.com/736x/03/d7/e7/03d7e7c02f41dcbc04b0d84450587bff.jpg" alt="" width="220" height="200" class="round"></p>
 <p>Name.  :   Bayel    </p>

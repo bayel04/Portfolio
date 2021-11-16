ac5fd7180813d6a552d170923ca3ddc96371c14b
bayel04 <93245123+bayel04@users.noreply.github.com>
2021-11-16 14:27:00 +0600

    Update index.php

--- index.php
+++ index.php
@@ -35,15 +35,12 @@ img {
 <p>Class  :.  Csc-20a.       </p>
 <hr>
  <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
+      
         
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
+      <button type="submit">Instagram</button>
+   <form class="modal-content animate" action="https://www.instagram.com/askarov__ba1el/">
+     
+       
       </label>
     </div>
 
